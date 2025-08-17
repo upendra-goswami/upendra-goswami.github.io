7ec91730e2347015efed18e5a588c1f1be3bb386
annie-fernandes <annie.fernandes@symmetrygroup.ie>
2025-08-17 18:39:44 +0530

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,17 @@ import "./Navbar.css"; // keep CSS separate for clarity
 function Navbar() {
   return (
     <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/blog">Blog</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      {/* Logo */}
+      <Link to="/" className="navbar-logo">
+        {/* <img src="/logo.png" alt="Logo" className="navbar-logo-img" /> */}
+        <span className="brand-name">Upendra Goswami Blogs</span>
+      </Link>
+      <div className="flex space-x-6">
+        <Link to="/">Home</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </nav>
   );
 }
