8fd0df91063b95ecac405acd148e596a95441521
annie-fernandes <annie.fernandes@symmetrygroup.ie>
2025-08-17 16:39:53 +0530

    change config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "upendra-goswami.github.io",
+  // base: "upendra-goswami.github.io",
 });
